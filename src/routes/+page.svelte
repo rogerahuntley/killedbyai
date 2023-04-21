cefe042948ecbaf86008a65265b2c65ae2002405
Roger Huntley II <roger.a.huntley@gmail.com>
2023-04-20 22:26:52 -0400

    add manifesto with link to testimonials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
 manifesto
+<br />
+to be written. AI is scary, and we're letting AI agents autonomously go on the internet. It's time to
+learn how to play guitar and hold our loved ones.
+<br />
+<a href="/testimonials">testimonials</a>
